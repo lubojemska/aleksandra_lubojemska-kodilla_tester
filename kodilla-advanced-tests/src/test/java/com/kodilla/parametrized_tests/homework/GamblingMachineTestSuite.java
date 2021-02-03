3fa78e9f2a1a65666818cff7dde92d2e82bc0b51
Aleksandra Lubojemska <o.lubojemska@gmail.com>
2021-02-03 10:13:28 +0100

    7.2

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -3,10 +3,12 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GamblingMachineTestSuite {
@@ -23,6 +25,17 @@ public class GamblingMachineTestSuite {
             set.add(Integer.parseInt(s));
         }
         assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(set));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6:20,25,35,1,18,35"}, delimiter = ':')
+    public void shouldReturnNumberOfWins(String input) throws InvalidNumbersException {
+        String[] rowSplit = input.split(",");
+        Set<Integer> set = new HashSet<>();
+        for (String s: rowSplit) {
+            set.add(Integer.parseInt(s));
+        }
+        assertEquals(true, gamblingMachine.howManyWins(set)!=1);
     }
 
 }
